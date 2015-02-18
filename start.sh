a35be2a344c0d893988855a24b2dfb02f3d8adac
Mohamed Meabed <mohamed.meabed@westwing.de>
2015-02-18 11:19:39 +0100

    Adding Docker data folder

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-B_DIR="`pwd`/"
 
-#chmod -R 777 $B_DIR/scripts/docker-data
+B_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DOCKER_DATA_FOLDER=$B_DIR/scripts/docker-data
+
+chmod -R 777 $DOCKER_DATA_FOLDER
 
 # cassandra
 cassandraNodeName=CASS01
@@ -32,13 +34,13 @@ if [ -n "${nutchRunning}" ]; then
     docker kill $nutchNodeName && docker rm $nutchNodeName
 fi
 
-cassandraId=$(docker run -d -P -v /Users/docker-data:/data:rw --name $cassandraNodeName meabed/cassandra)
+cassandraId=$(docker run -d -P -v $DOCKER_DATA_FOLDER:/data:rw --name $cassandraNodeName meabed/cassandra)
 cassandraIP=$(./scripts/ipof.sh $cassandraId)
 # -p 9200:9200
 # http://dockerhost:9200/_plugin/kopf/
 # http://dockerhost:9200/_plugin/HQ/
 
-esId=$(docker run -d -p 9200:9200 -P -v /Users/docker-data:/data:rw --name $esNodeName meabed/elasticsearch)
+esId=$(docker run -d -p 9200:9200 -P -v $DOCKER_DATA_FOLDER:/data:rw --name $esNodeName meabed/elasticsearch)
 esIP=$(./scripts/ipof.sh $esId)
 
-docker run -d -p 8899:8899 -P -e CASSANDRA_NODE_NAME=$cassandraNodeName -e ES_NODE_NAME=$esNodeName -it --link $esNodeName:$esNodeName --link $cassandraNodeName:$cassandraNodeName -v /Users/docker-data:/data:rw --name $nutchNodeName meabed/nutch
+docker run -d -p 8899:8899 -P -e CASSANDRA_NODE_NAME=$cassandraNodeName -e ES_NODE_NAME=$esNodeName -it --link $esNodeName:$esNodeName --link $cassandraNodeName:$cassandraNodeName -v $DOCKER_DATA_FOLDER:/data:rw --name $nutchNodeName meabed/nutch
