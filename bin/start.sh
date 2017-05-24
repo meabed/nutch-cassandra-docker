201f500cdf2023d4aaae30352ce710fbf8c5b36b
rootcss <shekhar003007@gmail.com>
2017-05-25 00:51:16 +0530

    start script fixes

--- bin/start.sh
+++ bin/start.sh
@@ -4,15 +4,16 @@ B_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DOCKER_DATA_FOLDER=$B_DIR/docker-data
 
 chmod -R 777 $DOCKER_DATA_FOLDER
-
+mkdir -p $DOCKER_DATA_FOLDER
 source "$B_DIR/nodes.sh"
-source "$B_DIR/stop.sh"
+# source "$B_DIR/stop.sh"
 
+echo "Starting cassaandra.."
 cassandraId=$(docker run -d -P -v $DOCKER_DATA_FOLDER:/data:rw --name $cassandraNodeName meabed/cassandra)
+echo "Finding IP of cassandra container.."
 cassandraIP=$("$B_DIR"/ipof.sh $cassandraId)
 
-# -p 9200:9200
-# http://dockerhost:9200/_plugin/kopf/
-# http://dockerhost:9200/_plugin/HQ/
-
-docker run -d -p 8899:8899 -P -e CASSANDRA_NODE_NAME=$cassandraNodeName -it --link $cassandraNodeName:$cassandraNodeName -v $DOCKER_DATA_FOLDER:/data:rw --name $nutchNodeName meabed/nutch:2.3
+echo "Starting Nutch container.."
+echo "cassandraNodeName: $cassandraNodeName"
+echo "nutchNodeName: $nutchNodeName"
+docker run -d -p 8899:8899 -P -e CASSANDRA_NODE_NAME=$cassandraNodeName -it --link $cassandraNodeName:$cassandraNodeName --net bridge -v $DOCKER_DATA_FOLDER:/data:rw --name $nutchNodeName meabed/nutch:2.3
